d353408b5d772018043f8032b2cdb2a69aa6cea3
Olga Akhunova <akhunovaolya7@rambler.ru>
2025-12-16 20:44:33 +0500

    changed TestBase.java

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,8 +1,11 @@
 package tests;
 
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+
 
 public class TestBase {
+    @BeforeEach
     public void setupEnvironment() {
         RestAssured.baseURI = "https://reqres.in";
         RestAssured.basePath="/api";
